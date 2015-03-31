a8f03e3bf8da171f8d727fb643967d342d8ecef9
Rey <rmmedillo@yahoo.com.ph>
2015-03-31 10:04:16 +0800

    03312015

---
+++ database/migrations/created/2015_03_31_091231_create_ProductCategory.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductCategory extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('ProductCategory', function($table) {
+			$table->increments('id');
+			$table->string('name', '150');
+			$table->timestamp('created_at');
+			$table->timestamp('updated_at');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
